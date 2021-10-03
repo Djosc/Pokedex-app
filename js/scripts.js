62dd4f5bd2eccc32cf048eaf485d79d76ef1d2c8
Djosc <davidjoscaldwell@gmail.com>
2021-10-02 21:43:37 -0400

    Created array with 3 objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello World');
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    {name: 'Charmander', height: 0.6, types: ['fire']},
+    {name: 'Squirtle', height: 0.5, types: ['water']}
+];
 
-let favFood = "steak";
-document.write(favFood);
