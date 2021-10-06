cfcd46fbe3c35f74c4ecdd00f2b4e91d1154a7b2
Djosc <davidjoscaldwell@gmail.com>
2021-10-05 22:36:28 -0400

    Added for loop to write pokemon to the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,34 @@ let pokemonList = [
     {name: 'Squirtle', height: 0.5, types: ['water']}
 ];
 
+for (let i = 0; i < pokemonList.length; i++) {
+    let name = pokemonList[i].name;
+    let height = pokemonList[i].height;
+    if (height > 0.6) {
+        document.write(`<p>${name} (height: ${height}m) - Wow that's big</p>`)
+    }
+    else {
+        document.write(`<p>${name} (height: ${height}m)</p>`)
+    }
+}
+
+
+/*  Playing around - seeing if i could do it with array.filter() or without a for loop.
+
+const largePokemon = pokemonList.filter(pokemon => pokemon.height > 0.6);
+
+console.table(largePokemon);
+
+pokemonList.forEach(writePokemon);
+
+function writePokemon(pokemon) {
+    const { name, height } = pokemon;
+    if (height > 0.6) {
+        document.write(`<p>${name} (height: ${height}m) - Wow that's big</p>`)
+    }
+    else {
+        document.write(`<p>${name} (height: ${height}m)</p>`)
+    }
+};
+
+*/
