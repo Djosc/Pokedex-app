4cbde96dfa899045c90484cafb05176870b9e9e4
Djosc <davidjoscaldwell@gmail.com>
2021-10-06 18:58:21 -0400

    Wrapped object array in IIFE and changed loop to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,38 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    {name: 'Charmander', height: 0.6, types: ['fire']},
-    {name: 'Squirtle', height: 0.5, types: ['water']}
-];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    let name = pokemonList[i].name;
-    let height = pokemonList[i].height;
-    if (height > 0.6) {
-        document.write(`<p>${name} (height: ${height}m) - Wow that's big</p>`)
+let pokemonRepository = (function () {
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+        { name: 'Charmander', height: 0.6, types: ['fire'] },
+        { name: 'Squirtle', height: 0.5, types: ['water'] }
+    ];
+
+    function add(pokemon) {
+        // if ((typeof pokemon === 'object') && Object.keys(pokemon).some(pokemon => 'name')) {
+        if ((typeof pokemon === 'object') && 'name', 'height', 'types' in pokemon) {
+            pokemonList.push(pokemon);
+        }
+        else {
+            console.log(`Pokemon does not contain expected values`);
+        }
     }
-    else {
-        document.write(`<p>${name} (height: ${height}m)</p>`)
+
+    function getAll() {
+        return pokemonList;
     }
-}
+
+    function find(pokemonName) {
+        return pokemonList.filter(pokemon => pokemon.name === pokemonName);
+    }
+
+    return {
+        add: add,
+        getAll: getAll,
+        find: find
+    };
+})();
 
 
-/*  Playing around - seeing if i could do it with array.filter() or without a for loop.
-
-const largePokemon = pokemonList.filter(pokemon => pokemon.height > 0.6);
-
-console.table(largePokemon);
-
-pokemonList.forEach(writePokemon);
-
+// Takes a pokemon object and writes it to the DOM while marking larger pokemon
 function writePokemon(pokemon) {
     const { name, height } = pokemon;
     if (height > 0.6) {
@@ -34,4 +43,15 @@ function writePokemon(pokemon) {
     }
 };
 
-*/
+// Calls the writePokemon function on all objects in pokemonRepository.pokemonList
+pokemonRepository.getAll().forEach(writePokemon);
+
+
+
+console.log(Object.keys(pokemonRepository));
+
+pokemonRepository.add({ name: 'Pikachu', height: 0.2, types: ['electric'] });
+
+console.log(pokemonRepository.getAll());
+
+console.log(pokemonRepository.find('Bulbasaur'));
