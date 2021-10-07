55b082bd7c6b44cc3b3f0647b269e7539c6e243a
Djosc <davidjoscaldwell@gmail.com>
2021-10-06 22:59:13 -0400

    Revert "Trying out jsdoc comments"
    This reverts commit 4055adc82b44a7a31567abef343591e7d164b1e2.

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,6 @@ let pokemonRepository = (function () {
         { name: 'Squirtle', height: 0.5, types: ['water'] }
     ];
 
-    /**
-     * Takes a pokemon object and checks if it contains the correct properties,
-     * then adds it to the {@link pokemonList} array
-     * 
-     * @param {Object} pokemon - pokemon object
-     * @param {string} pokemon.name - pokemon name
-     * @param {number} pokemon.height - the pokemon's height
-     * @param {string[]} pokemon.types - array of pokemon types
-     */
     function add(pokemon) {
         if ((typeof pokemon === 'object') &&
             'name' in pokemon && 'height' in pokemon && 'types' in pokemon) {
@@ -40,11 +31,7 @@ let pokemonRepository = (function () {
     };
 })();
 
-/**
- * Takes a pokemon object and writes it to the DOM while marking larger pokemon
- * 
- * @param {object} pokemon - pokemon object
- */
+// Takes a pokemon object and writes it to the DOM while marking larger pokemon
 function writePokemon(pokemon) {
     const { name, height } = pokemon;
     if (height > 0.6) {
